c23e8f8f905fbba991c5e17298c7e4534d3fc79a
tonypey <166833585+tonypey@users.noreply.github.com>
2024-06-19 18:29:21 -0400

    Initial Commit

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+    'mongodb+srv://root:root@books-store-mern.jcryvue.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Books-Store-MERN'
+
+//please create a free database for yourself
+//this database will be deleted after tutorial
